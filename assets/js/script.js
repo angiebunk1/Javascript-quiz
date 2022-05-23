e4e03aaf1008d80ec32e889e12a1505eb21a547e
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-05-22 19:50:24 -0500

    added event listeners to function that don't work yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@ var time = 120;
 var initials = "";
 
 
-
 var beginQuiz = function () {
     document.getElementById("h2").innerHTML = "Question 1";
     document.getElementById("btn1").innerHTML = "Answer 1";
+
 
     var btn2 = document.createElement("button");
     btn2.innerHTML = "Answer 2";
@@ -16,12 +16,24 @@ var beginQuiz = function () {
     btn3.innerHTML = "Answer 3";
     document.getElementById("section").appendChild(btn3);
 
+    btn1.addEventListener("click", function() {
+        score = score + 20;
+        question2();
+    });
 
-}
+    btn2.addEventListener("click", function(){
+        time = time - 10;
+        question2();
+    });
 
-var beginBtn = document.querySelector("#btn1");
+    btn3.addEventListener("click", function(){
+        time = time - 10;
+        question2();
+    });
+    
+};
 
 
+var btn1 = document.querySelector("#btn1");
 
-
-beginBtn.addEventListener("click", beginQuiz);
+btn1.addEventListener("click", beginQuiz);
