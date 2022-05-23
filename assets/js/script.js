808b44463686768a2647195f68bb87bb3a6d1154
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-05-22 23:19:52 -0500

    added a few more functions, working on last screen now

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,17 @@ var beginQuiz = function () {
     timer();
 
     document.getElementById("h2").innerHTML = "Question 1";
-    document.getElementById("btn1").innerHTML = "Answer 1";
+    btn1.innerHTML = "Answer 1.1";
 
     btn1.removeEventListener("click", beginQuiz);
 
 
-    var btn2 = document.createElement("button");
-    btn2.innerHTML = "Answer 2";
+    btn2 = document.createElement("button");
+    btn2.innerHTML = "Answer 1.2";
     document.getElementById("section").appendChild(btn2);
 
-    var btn3 = document.createElement("button");
-    btn3.innerHTML = "Answer 3";
+    btn3 = document.createElement("button");
+    btn3.innerHTML = "Answer 1.3";
     document.getElementById("section").appendChild(btn3);
 
     btn1.addEventListener("click", function() {
@@ -34,6 +34,7 @@ var beginQuiz = function () {
         time = time - 10;
         question2();
     });
+
     
 };
 
@@ -46,7 +47,106 @@ var timer = function() {
     }, 1000);
 };
 
+var question2 = function () {
+    
+    document.getElementById("h2").innerHTML = "Question 2";
+    btn1.innerHTML = "Answer 2.1";
+    btn2.innerHTML = "Answer 2.2";
+    btn3.innerHTML = "Answer 2.3";
+
+
+    btn1.removeEventListener("click", function() {
+        score = score + 20;
+        question2();
+    });
+
+    btn2.removeEventListener("click", function(){
+        time = time - 10;
+        question2();
+    });
+
+    btn3.removeEventListener("click", function(){
+        time = time - 10;
+        question2();
+    });
+
+    
+    btn1.addEventListener("click", function() {
+        time = time - 10;
+        question3();
+    });
+
+    btn2.addEventListener("click", function(){
+        score = score + 20;
+        question3();
+    });    
+    
+    btn3.addEventListener("click", function() {
+        time = time - 10;
+        question3();
+    });
+};
+
+var question3 = function () {
+    
+    document.getElementById("h2").innerHTML = "Question 3";
+    btn1.innerHTML = "Answer 3.1";
+    btn2.innerHTML = "Answer 3.2";
+    btn3.innerHTML = "Answer 3.3";
+
+
+    btn1.removeEventListener("click", function() {
+        time = time - 10;
+        question3();
+    });
+
+    btn2.removeEventListener("click", function(){
+        score = score + 20;
+        question3();
+    });
+
+    btn3.removeEventListener("click", function(){
+        time = time - 10;
+        question3();
+    });
+
+    
+    btn1.addEventListener("click", function() {
+        time = time - 10;
+        endQuiz();
+    });
+
+    btn2.addEventListener("click", function(){
+        time = time - 10;
+        endQuiz();
+    });    
+    
+    btn3.addEventListener("click", function() {
+        score = score + 20;
+        endQuiz();
+    });
+};
+
+var endQuiz = function () {
+    clearInterval(function(){
+        document.getElementById("timer").innerHTML="Seconds remaining: " + time; time--;
+        if (time <= 0) {
+            endQuiz();
+        }
+    }, 1000);
+
+    document.getElementById("h2").innerHTML = "Quiz is finished. Enter your initials to log your score.";
+
+    
+
+};
+
+
 
 var btn1 = document.querySelector("#btn1");
+
+var btn2 = "";
+
+var btn3 = "";
 
 btn1.addEventListener("click", beginQuiz);
