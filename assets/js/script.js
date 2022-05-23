8e87aff730a2cccf17bf8f81ff9dd95c0c12946f
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-05-22 19:18:07 -0500

    adding first function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,26 @@ var score = 0;
 var time = 120;
 var initials = "";
 
-console.dir(window.document);
 
 
 var beginQuiz = function () {
     document.getElementById("h2").innerHTML = "Question 1";
+    document.getElementById("btn1").innerHTML = "Answer 1";
+
+    var btn2 = document.createElement("button");
+    btn2.innerHTML = "Answer 2";
+    document.getElementById("section").appendChild(btn2);
+
+    var btn3 = document.createElement("button");
+    btn3.innerHTML = "Answer 3";
+    document.getElementById("section").appendChild(btn3);
+
+
 }
 
 var beginBtn = document.querySelector("#btn1");
+
+
+
 
 beginBtn.addEventListener("click", beginQuiz);
